2cc57fc45a830876f189b959861194f8facf11b7
Zaida Kodzoeva <zkod777@gmail.com>
2022-10-01 18:19:08 +0400

    learned about arrays and tuples

--- src/tuples.rs
+++ src/tuples.rs
@@ -1,3 +1,7 @@
+// Tuples group together values of different types
+// Max 12 elements
+
 pub fn run(){
-    
+    let person: (&str, &str, i8) = ("John", "Spain", 37); // adding types
+    print!("{} is from {} and is {}", person.0, person.1, person.2);
 }
