b0480a7107b030ab8c07d7a2f0538e269a19274f
Zaida Kodzoeva <zkod777@gmail.com>
2022-10-02 00:57:04 +0400

    working on vectors 59:21

---
+++ src/vectors.rs
@@ -0,0 +1,44 @@
+// Vectors - Resizable arrays
+use std::mem;
+
+pub fn run(){
+    let mut numbers: Vec<i32> = vec![1, 2, 3, 4, 5];
+
+    // Re-assign value
+    numbers[2] = 20;
+
+    // Add on to vector
+    numbers.push(6);
+    numbers.push(7);
+
+    // Pop off last value
+    numbers.pop();
+
+    // Print all values
+    println!("{:?}", numbers);
+
+    // Get single val
+    println!("Single Value: {}", numbers[0]);
+
+    // Get vector length
+    println!("Vector Length: {}", numbers.len());
+
+    // Vector are stack allocated
+    println!("Vector occupies {} bytes", mem::size_of_val(&numbers));
+
+    // Get Slice
+    let slice: &[i32] = &numbers[0..2]; 
+    println!("Slice: {:?}", slice);
+
+    // Loop through vector values
+    for x in numbers.iter(){
+        println!("Number: {}", x);
+    }
+
+    // Loop & mutate values
+    for x in numbers.iter_mut(){
+        *x *= 2;
+    }
+
+
+}
